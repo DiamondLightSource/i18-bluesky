784776a553971cb6a69f0a537392a3ebea91bc65
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-08-02 11:33:08 +0100

    fix one import

--- src/i18_bluesky/devices/sim_epics_devices.py
+++ src/i18_bluesky/devices/sim_epics_devices.py
@@ -1,5 +1,6 @@
-from devices.device_utils import *
 import os
+
+from i18_bluesky.devices.device_utils import create_epics_motor
 
 # Make sure EPICS_CA_SERVER_PORT is set to correct value (6064 for DLS sim area detector and motors, 5064 on beamlines)
 os.environ['EPICS_CA_SERVER_PORT'] = "6064"
