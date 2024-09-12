95046f66e55d807e2cc8196e93cd1867a9bc2b51
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-09-12 10:58:09 +0100

    respond to feedback

--- src/i18_bluesky/plans/grid_scan_diode.py
+++ src/i18_bluesky/plans/grid_scan_diode.py
@@ -1,9 +1,8 @@
-import bluesky.plan_stubs as bps
 import bluesky.plans as bp
-import bluesky.preprocessors as bpp
 from dodal.common import MsgGenerator, inject
 from dodal.devices.i18.diode import Diode
 from dodal.devices.i18.table import Table
+from dodal.plans.data_session_metadata import attach_data_session_metadata_decorator
 
 DIODE = inject("diode")
 
@@ -11,56 +10,19 @@ TABLE = inject("table")
 
 
 # todo make it use scanspec instead
+
+
+@attach_data_session_metadata_decorator()
 def grid_scan_diode(
-    start_x: float,
-    steps_x: int,
-    start_y: float,
-    steps_y: int,
+    x_start: float,
+    x_stop: float,
+    x_steps: int,
+    y_start: float,
+    y_stop: float,
+    y_steps: int,
     diode: Diode = DIODE,
     table: Table = TABLE,
 ) -> MsgGenerator:
-    """
-    Scan wrapping `bp.grid_scan`
-
-    Args:
-        detectors: List of readable devices, will take a reading at each point
-        motor1: name of motor to be moved
-        scan_args1: [start, stop, step]
-        motor2: name of motor to be moved
-        scan_args2: [start, stop, step]
-        metadata
-        snake_axes: if True, the second axis will reverse direction
-    """
-    detectors = [diode]
-
-    plan_args = {
-        "diode": repr(diode),
-        "steps_x": 5,
-        "steps_y": 5,
-        "start_x": 0,
-        "start_y": 0,
-    }
-
-    _md_ = {
-        "detectors": list(map(repr, detectors)),
-        "plan_name": "grid_scan",
-        "plan_args": plan_args,
-        "shape": [steps_x, steps_y],
-    }
-
-    @bpp.stage_decorator(detectors)
-    @bpp.run_decorator(md=_md_)
-    def inner_grid_scan_plan():
-        # todo move to the starting position
-        yield from bps.mv(table, start_x, start_y)
-        # todo make sure this is right
-        yield from bp.grid_scan(detectors, md=_md_)
-        # todo consider commented out variant
-        # todo consider linspace and numpy instead
-        yield from bps.read(detectors)
-        # for x in range(steps_x):
-        #     for y in range(steps_y):
-        #         yield from bps.mv(table, start_x + x, start_y + y)
-        #         yield from bps.trigger_and_read(detectors)
-
-    yield from inner_grid_scan_plan()
+    yield from bp.grid_scan(
+        [diode], table, x_start, x_stop, x_steps, y_start, y_stop, y_steps
+    )
